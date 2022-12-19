e6ba3c3fb1a69f5976567b85c59e075a4ea5b0af
izzett222 <iizzeddin62@gmail.com>
2022-12-19 09:00:22 +0200

    feat: add nextjs and tailwindcss

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '../styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <h1 className='text-3xl text-red-300'>IN PROGRESS</h1>
+    
+  )
+}
